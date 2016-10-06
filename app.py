89539fd96d43f4b67c2bfcd80427ec282007d296
rtyabgzdhf <rty1991@gmail.com>
2016-10-06 15:42:18 +0700

    12354

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 from flask import Flask
-from redis import Redis
 
 app = Flask(__name__)
-redis = Redis(host='redis', port=6379)
 
 @app.route('/')
 def hello():
-    redis.incr('hits')
-    return 'Hello World! abz sdskd Seen %s times.' % redis.get('hits')
+    return 'Hello World! abz sdskd.' 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
