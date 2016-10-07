ab71736f58cad5818ed8b0c4a6eb9e02aef569b8
rtyabgzdhf <rty1991@gmail.com>
2016-10-07 10:29:13 +0700

    time

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World! abz sdskd test 5678' 
+    return 'Hello World! abz sdskd test 5678 at ' + datetime.datetime.now
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
